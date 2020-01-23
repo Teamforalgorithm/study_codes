c15b4d4ddb0b31b2ae6f06090b03d88a75a999ca
Kim Jihyeong <kimjihyeong200@gmail.com>
2020-01-23 15:38:11 +0900

    editing codes

--- kjh/parametric/2805.py
+++ kjh/parametric/2805.py
@@ -1,4 +1,3 @@
-# WRONG
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
@@ -6,14 +5,29 @@ arr = list(map(int, sys.stdin.readline().split()))
 
 arr = list(reversed(sorted(arr)))
 
+# print(arr)
+
 acquired = 0
 count = 1
+identical = 1
 
 for i, j in zip(arr, arr[1:] + [0]):
     if (i - j) == 0:
+        identical += 1
+        count += 1
         continue
+
     acquired += (i - j) * count
+    # print(acquired)
+
     if acquired >= M:
-        print(j)
+        if identical > 1:
+            # print(identical)
+            # print('j: ', j)
+            # print('acquired_sum: ', ((acquired - M) // (identical + 1)))
+            print(j + ((acquired - M) // (identical + 1)))
+        else:
+            print(j)
         break
+    identical = 1
     count += 1
