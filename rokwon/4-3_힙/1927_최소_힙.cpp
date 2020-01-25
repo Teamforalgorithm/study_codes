13cf905239e55e5216c5ab15be75f8827859aadd
RokwonK <wonrok97@naver.com>
2020-01-25 22:33:07 +0900

    6th study

---
+++ rokwon/4-3_힙/1927_최소_힙.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<functional>
+ 
+using namespace std;
+ 
+int main(void){
+    
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, num = 0;
+    priority_queue< int,vector<int>,greater<int> > heap;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        if (num != 0)  {
+            heap.push(num);
+        }
+        else {
+            if(heap.empty()) {
+                cout << 0 << '\n';
+            }
+            else {
+                cout << heap.top() << '\n';
+                heap.pop();
+            }
+        }
+        
+    }
+    
+    return 0;
+}
