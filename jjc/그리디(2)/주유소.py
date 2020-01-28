3bf0790f887abd2bed8575d00b47e56f1fb9ed30
anjwoc <anjwoc@naver.com>
2020-01-28 16:33:14 +0900

    sv

--- jjc/그리디(2)/주유소.py
+++ jjc/그리디(2)/주유소.py
@@ -1,10 +1,19 @@
 import sys
+import heapq
 input = lambda: sys.stdin.readline().strip()
 
 n = int(input())
 ans = 0
 road = list(map(int, input().split()))
+heap = []
+price = list(map(int, input().split()))
+print(price)
+print(road)
+for i in range(n):
+  heapq.heappush(heap, price[i])
+  ans += (heapq.heappop(heap) * road[i])
 
+print(ans)
 
 
 
